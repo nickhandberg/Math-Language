d47d03ba2fee24e8ff84b46581430d0e10fddc4e
noko12312 <42224565+noko12312@users.noreply.github.com>
2022-04-23 14:51:55 -0500

    Added some comments to lexer.rb

--- lib/calc/lexer.rb
+++ lib/calc/lexer.rb
@@ -2,19 +2,19 @@ require 'strscan'        # require String Scanner
 require 'calc/token'     #require calc/token classes
 
 module Calc
-  # A class that takes lines of source code and creates an ordered array of Calc::Token .
+  # Token class takes the Calc code and generates a meaningful set of tokens that can be parsed
   class Lexer
 
-    # Converts the string value to a real number.
+    # String to literal conversion 
     def self.convert_to_literal(str)
       if /(\+|\-)?\d+\.\d+/ =~ str    #if the input value is a String
-        str.to_f                      #convert String to a fload
+        str.to_f                      #convert String to a float
       else
-        str.to_i                      #conver String to integer
+        str.to_i                      #convert String to integer
       end
     end
 
-    # Takes the input lines of source code and produces an array of Calc::Token
+    # Generates tokens from the Calc code
     def self.lex(input)   #lexer get input line of source code
       tokens = []         #create token input array
 
@@ -22,45 +22,51 @@ module Calc
 
       definitions_done = false
       input.each_line do |line|
-        source_line = line.strip # Remove leading and trailing spaces
+        source_line = line.strip
 
-        next if source_line.empty? # Skip empty lines
-
+        # Skips the line if empty
+        next if source_line.empty?
+        
+        # Checks for correct syntax in the code. (Checks for parenthesis, unsupported symbols, and late definitions)
         Calc::invalid_syntax("",0, "Each line must start with '('") unless source_line.start_with?('(') #
         Calc::invalid_syntax("",0, "Each line must end with ')'") unless source_line.end_with?(')')
         Calc::invalid_syntax("",0, "Syntax contains symbols that are not supported") if /[^\(\)a-zA-Z\d\s\+\-\*\/\.]+/ =~ source_line
         Calc::invalid_syntax("",0, "Definitions must come before the expressions") unless !definitions_done || !/^\(\s*let/.match(source_line)
 
+        # Checking for definitions (includes LET keyword)
         if /^\(\s*(?<let>let)\s*(?<identifier>[a-zA-Z][a-zA-Z\d]*)\s*(?<number>(\+|\-)?(\d+\.\d+|\d+))\)$/ =~ source_line
-          # We've found a definition, let's create a set of Tokens.
+          # Creates the tokens for the definition
           tokens << Token.new(Token::LET,let,1)
           tokens << Token.new(Token::IDENTIFIER,identifier,5)
           tokens << Token.new(Token::NUMBER, convert_to_literal(number),5 + identifier.length - 1)
+
+        # Otherwise, definitions are done, so move on to expressions
         else
           definitions_done = true
           last_pos = 0
           s = StringScanner.new(source_line)
           while (!s.eos?) do
-            s.skip(/\(/) # Skip (
-            s.skip(/\)/) # Skip )
+            # Ignores the opening/closing parenthesis by skipping
+            s.skip(/\(/) 
+            s.skip(/\)/) 
 
-            # Scan through the string to find a number.
+            # Finds number by scanning through the string and creates new token for it
             if number = s.scan(/(\+|\-)?(\d+\.\d+|\d+)/)
               tokens << Token.new(Token::NUMBER,convert_to_literal(number),s.pos)
             end
 
-            # Scan through the string to find an identifier.
+            # Finds identifier by scanning through the string and creates a new token for it
             if identifier = s.scan(/[a-zA-Z][a-zA-Z\d]*/)
               tokens << Token.new(Token::IDENTIFIER,identifier,s.pos)
             end
 
-            # Scan through the string to find an operator.
+            # Finds the operator by scanning through the string and creates a new token for it
             if operator = s.scan(/\+|\-|\*|\//)
               tokens << Token.new(Token::OPERATOR,operator,s.pos)
             end
 
             s.skip(/\s+/)
-            Calc::invalid_syntax(s.post_match,s.pos, "") if last_pos == s.pos # We didn't scan anything
+            Calc::invalid_syntax(s.post_match,s.pos, "") if last_pos == s.pos
             last_pos = s.pos
           end
         end
